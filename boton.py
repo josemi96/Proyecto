b0eaeca1a6667c3cb8324bc5482cf7545e5a041d
josemi96 <hidalgorondon96@gmail.com>
2018-05-10 01:31:53 +0200

    mayo

--- boton.py
+++ boton.py
@@ -4,14 +4,18 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13,GPIO.IN)
 prev_input = 0
-ok=True
+Encendido=False
 
-while ok:
+while True:
   input = GPIO.input(13)
   if ((not prev_input) and input):
-     pid_a = subprocess.Popen(["python","/home/pi/Proyecto/Version1.py"])
-     ok=False
+     p = subprocess.Popen(["python","/home/pi/Proyecto/Version1.py"])
+     Encendido = True
   prev_input = input
   #Pequena pausa
-  time.sleep(0.05)
-exit(0)
+  time.sleep(0.5)
+  if( Encendido == True ):
+     while p.poll() is None:
+        time.sleep(0.5)
+     exit(0)
+  
